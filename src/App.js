3c36f9b3716b7bed69bfd18addca05b53ba85119
piyal-das <66130612+piyal-das@users.noreply.github.com>
2020-09-06 22:21:37 +0600

    post-comment-done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-// import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/Home/Home';
 
 import {
@@ -10,13 +9,11 @@ import {
   Link
 } from "react-router-dom";
 import NotFound from './components/NotFound/NotFound';
-// import Header from './components/Header/Header';
 import PostDetails from './components/PostDetails/PostDetails';
 
 function App() {
   return (
     <div>
-      {/* <Header></Header> */}
       <Router>
           <Switch>
               <Route path='/home'>
